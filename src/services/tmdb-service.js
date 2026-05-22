a17fca02dd74fdb8ce2a18ea160948330a9fbac2
Carlos del Prado <carlos.delpradomota@telefonica.com>
2026-05-22 19:41:27 +0200

    feat: integrate TMDB API to fetch and parse movies

---
+++ src/services/tmdb-service.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import genres from '../data/genres.json';
+
+const http = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
+  } 
+});
+
+const imagesBaseUrl = 'https://image.tmdb.org/t/p/original';
+
+
+function parseMovie(movie) {
+
+  function toGenre(id) {
+    const [genre,] = Object.entries(genres)
+      .find(([name, genreId]) => id === genreId) ?? [];
+    return genre;
+  }
+
+  return {
+    id: movie.id,
+    title: movie.title,
+    overview: movie.overview,
+    poster: `${imagesBaseUrl}${movie.poster_path}`,
+    backdrop: `${imagesBaseUrl}${movie.backdrop_path}`,
+    releaseDate: movie.release_date,
+    rating: movie.vote_average,
+    genres: movie.genre_ids.map((id) => toGenre(id)).filter((genre) => genre)
+  }
+}
+
+
+export async function listMovies(genre) {
+  const params = {}
+  if (genre) {
+    params.with_genres = genres[genre]
+  }
+  const { data } = await http.get('/discover/movie', { params });
+  return data.results?.map((movie) => parseMovie(movie)) ?? [];
+}
